87fc598421c1be08d5ce0bdab41ed86139e4226e
Sophonias Demiss <sophidemiss@gmail.com>
2024-05-03 20:32:46 +0300

    improved the icons

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -15,11 +15,11 @@ const Hero = () => {
             
             <img className=' w-52 md:w-80 mx-auto rounded-3xl' src={pic} alt="" />
             <ul className=' flex justify-between w-1/2 mx-auto my-7'>
-              <li><a href="https://github.com/Sophinaz?tab=repositories"><img className=' w-11 rounded-full' src={github} alt="" /></a></li>
-              <li><a href="https://www.instagram.com/sophinaz_/?hl=en"><img className=' w-11 rounded-full' src={insta} alt="" /></a></li>
-              <li><a href="https://www.linkedin.com/in/sophonias-demiss-3020b62ab/"><img className=' w-11 rounded-full' src={linkedin} alt="" /></a></li>
-              <li><a href="sophidemiss@gmail.com"><img className=' w-11 rounded-full' src={mail} alt="" /></a></li>
-              <li><img className=' w-11 rounded-full' src={telegram} alt="" /></li>
+              <li><a href="https://github.com/Sophinaz?tab=repositories"><img className=' transform hover:scale-125 duration-300 w-11 rounded-full' src={github} alt="" /></a></li>
+              <li><a href="https://www.instagram.com/sophinaz_/?hl=en"><img className=' transform duration-300 hover:scale-125 w-11 rounded-full' src={insta} alt="" /></a></li>
+              <li><a href="https://www.linkedin.com/in/sophonias-demiss-3020b62ab/"><img className=' transform duration-300 hover:scale-125 w-11 rounded-full' src={linkedin} alt="" /></a></li>
+              <li><a href="sophidemiss@gmail.com"><img className=' transform duration-300 hover:scale-125 w-11 rounded-full' src={mail} alt="" /></a></li>
+              <li><img className=' transform duration-300 hover:scale-125 w-11 rounded-full' src={telegram} alt="" /></li>
             </ul>
             <h2 className='text-2xl my-6'>Software and web Developer</h2>
         </div>
@@ -29,7 +29,7 @@ const Hero = () => {
             <p>As a front-end web developer and Electrical and Computer Engineering at AAiT, I have craftes my skills in javascript and python languages.
                 I have passion for creative works and developments. 
                 In here you will find almost everything about me. Try to about me here on this site and if you are intersted contact me.</p>
-        <h2 className='p-2 mx-auto cursor-pointer bg-slate-400 w-24 rounded-full'>Contact?</h2>
+        <h2 className='p-2 mx-auto cursor-pointer bg-slate-400 w-24 rounded-full'><a href="https://www.linkedin.com/in/sophonias-demiss-3020b62ab/"> Contact?</a></h2>
         </div>
     </div>
   )
